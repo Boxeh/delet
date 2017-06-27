4fff21991098c98267a543e437129ef034295e5e
Suvan <suvan@outlook.com>
2017-06-27 22:19:47 +0100

    Add tokens.js
    Returns a HiveMC player's tokens, by reading the message author's Discord username.

---
+++ commands/tokens.js
@@ -0,0 +1,22 @@
+exports.run = (client, message, author) => {
+    const hive = require('hive-api');
+
+let player = new hive.Player(`${message.author.username}`);
+
+player.info().then(info => {
+    message.channel.send(`Tokens: ${info.tokens}`)
+});
+
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: "tokens",
+    description: "Returns your amount of tokens on the HiveMC Network.",
+    usage: "tokens"
+};
