5fe35a9912db4e35a3d54d4d731964659e82389f
Suvan <suvan@outlook.com>
2017-06-29 23:15:52 +0100

    Create delet-bootstrap.sh

---
+++ delet-bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install Node.js and NPM from latest.
+# From nodesource (Chris Lea PPA) as Ubuntu packaging is somewhat behind.
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+# Install Nodemon globally and locally.
+sudo npm install --global nodemon
+sudo npm install --save nodemon
+
+# Source home directory.
+home='/home/Discord'
+
+# NPM build stuff.
+mkdir $home/node_modules
+cd $home/bot
+ln -s $home/node_modules/ $home/bot/node_modules
+npm install
+
+# Daemonises an instance.
+nodemon $home/bot/bin/app.js
