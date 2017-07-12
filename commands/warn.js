7e9224bc43b91fdb478fc19f90c6696898926ab3
Suvan <suvan@outlook.com>
2017-07-12 16:34:52 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,23 +1,49 @@
 const Discord = require('discord.js');
-exports.run = (client, message, args) => {
+exports.run = (client, message, args, guild) => {
     let reason = args.slice(1).join(' ');
     let user = message.mentions.users.first();
     let modlog = client.channels.find('name', 'delet-this');
     if (!modlog) return message.reply('I cannot find a mod-log channel!')
     if (reason.length < 1) return message.reply('you must supply a reason for the warning.');
     if (message.mentions.users.size < 1) return message.reply('you must mention a user to warn.').catch(console.error);
-    const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setDescription(`**Action:** Warning\n**Target:** ${user.tag}\n**Responsible moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
-    return client.channels.get(modlog.id).sendEmbed(embed);
+    client.channels.get(modlog.id).send({embed: {
+        color: 16381497,
+        author: {
+        name: `⚠️ Warning issued to ${user.username}#${user.discriminator} (${user.id})`,
+        icon_url: user.avatarURL
+        },
+        title: "",
+        url: "",
+        description: `\`\`\`js\nReason: ${reason}\nResponsible moderator: ${message.author.tag} (${message.author.id})\`\`\``,
+        // fields: [{
+        //     name: "Fields",
+        //     value: "They can have different fields with small headlines."
+        // },
+        // {
+        //     name: "Masked links",
+        //     value: "You can put [masked links](http://google.com) inside of rich embeds."
+        // },
+        // {
+        //     name: "Markdown",
+        //     value: "You can put all the *usual* **__Markdown__** inside of them."
+        // }
+        // ],
+        timestamp: new Date(),
+        footer: {
+        //icon_url: client.user.avatarURL,
+        text: "Warnings are not currently tracked"
+        }
+    }
+});
+message.channel.send(':ok_hand:');
+user.send(`⚠️ You were warned in ${message.guild.name} for the reason "${reason}". Please ensure you follow all rules that apply to you in the server/guild as being warned multiple times can lead to you being banned or kicked.`);
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
-    permLevel: 2
+    permLevel: 0
 };
 
 exports.help = {
