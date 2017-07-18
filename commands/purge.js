4fc728d1b4bf5bec62d6e5ace600c73d2b2075a2
Suvan <suvan@outlook.com>
2017-07-18 20:43:08 +0100

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,15 +1,28 @@
+const settings = require('../settings.json');
 exports.run = function(client, message, args) {
+    let modRole = message.guild.roles.find('name', 'delet Mod');
     let messagecount = parseInt(args.join(' '));
+    if (!message.member.roles.has(modRole.id)) {
+        return message.reply('you have insufficient permissions to use this command.').catch(console.error);
+    }
     message.channel.fetchMessages({
         limit: messagecount
     }).then(messages => message.channel.bulkDelete(messages));
+
+    message.channel.send('Messages purged.')
+        .then(msg => {
+            msg.delete()
+        });
+
+        if (message.content.startsWith(settings.prefix + 'purge')) message.delete()
+
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
-    permLevel: 2
+    permLevel: 0
 };
 
 exports.help = {
